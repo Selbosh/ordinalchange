2332e9480d5d62579b648f3b1f547760b1c2e125
David Selby <david@selbys.org.uk>
2022-09-02 16:38:07 +0200

    Add Stan optimization

--- R/cusum.R
+++ R/cusum.R
@@ -35,27 +35,51 @@ bartlett_eta <- function(X) {
     return(eta2)
 }
 
-#' CUSUM test statistics
+#' Detect changepoints using CUSUM test statistics
+#' 
+#' This function calculates a CUSUM test statistics at various time points and returns the highest one found.
+#' This value can then be compared with (empirical) percentiles of the distribution of the test statistic,
+#' to decide whether or not a changepoint has been found.
+#' 
+#' @return 
+#' A vector containing the position of the best changepoint (\eqn{\tau}) and the respective value of the test statistic (\eqn{M}).
+#' 
+#' @param Y Integer vector of responses.
+#' @param L Period of the sinusoidal component of the latent process.
+#' @param K Number of ordinal categories. \code{Y} is assumed to take on values \eqn{1, \dots, K}.
+#' @param t Candidate positions for the changepoints. Defaults to \eqn{2, \dots, n - 1}.
 #' 
 #' @source Section 3 of \url{https://doi.org/10.1002/env.2752}
 #' 
 #' @rdname cusum
-M_X <- function(Y, mu, cutpoints) {
-    X_tilde <- X_tilde(Y, mu, cutpoints)
-    eta <- sqrt(bartlett_eta(X_tilde))
-    n <- length(Y)
-    maximum <- -Inf
-    best_tau <- 0
-    for (tau in 1:n) {
-        test <- abs(sum(X_tilde - tau / n * sum(X_tilde)) / sqrt(n))
-        if (test > maximum) {
-            maximum <- test
-            best_tau <- tau
-        }
+#' @export
+changepoint_detection <- function(Y, L, K = max(Y), t = 2:(length(Y) - 1),
+                                  verbose = FALSE) {
+  n <- length(Y)
+  maximum <- -Inf
+  best_changepoint <- NA
+  for (tau in t) {
+    if (verbose)
+        message('Fitting model for timepoint tau = ', tau)
+    model <- seq_estimation(Y, tau = tau, K = K, L = L)
+    theta <- model$theta
+    rho <- model$rho$par
+    mu <- ordinalchange:::mu(alpha0 = theta[K-1],
+                             alpha1 = theta[K],
+                             A = theta[K + 1],
+                             D = theta[K + 2],
+                             Delta = theta[K + 3],
+                             Y, tau, L = L)
+    X_tilde <- ordinalchange:::X_tilde(Y, mu, cutpoints = c(-Inf, 0, theta[1:(K-2)], Inf))
+    eta <- sqrt(ordinalchange:::bartlett_eta(X_tilde))
+    test_stat <- abs(sum(X_tilde[1:tau]) - tau / n * sum(X_tilde)) / sqrt(n)
+    if (test_stat > maximum) {
+        if (verbose)
+            message('\tBetter changepoint found: tau = ', tau,
+                    ' with M = ', test_stat)
+        maximum <- test_stat
+        best_changepoint <- tau
     }
-    c(M = maximum / eta, tau = best_tau)
+  }
+  c(tau = best_changepoint, M = maximum)
 }
-
-#' Detect changepoints using CUSUM test statistics
-#' 
-#' 
